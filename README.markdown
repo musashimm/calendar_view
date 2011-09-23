d11dd8f55bccf1182fafbf812b0bc1dd987d0e9b
Wojciech Todryk <wojciech@todryk.pl>
2011-09-23 20:20:51 +0200

    some extras for calendar

--- README.markdown
+++ README.markdown
@@ -1,16 +1,16 @@
 ## Introduction
-_Calendar view_ is a rails plugin which extends application of calendar view.
+_Calendar view_ is a rails plugin which extends application of calendar views.
 
 ## Requirements
 
-Plagin was built as Rails 3 Engine plugin.
+Plugin was built as Rails 3 Engine plugin.
 
 ## Usage
 
 Add to Gemfile of application:
 
 ```ruby
-gem "calendar_view"
+gem "calendar_view", "~> 0.0.3"
 ```
 
 than
@@ -19,9 +19,33 @@ than
 bundle install
 ```
 
+to install gem.
+
 In views of Your application add:
 
 ```ruby
-<%= calendar_cube %>
+<% 1.upto(6) do |i| %>
+<%=  calendar_square(:month=>i,:highlight_between => Date.new(2011,9,19)..Date.new(2011,9,25)) %>
+<% end %>
+<%= calendar_square %>
+<%= calendar_square(:month_delta=>-12,:year_delta=>-1) %>
+<%= calendar_square(:month_delta=>12) %>
+<%= calendar_square(:month=>12,:highlight_date => Date.new(2011,12,2)) %>
+<%= calendar_square(:month=>1,:first_wday=>0) %>
+<%= calendar_square(
+                   :month => 1,                                                    # month
+                   :year => 2011,                                                  # year
+                   :month_delta => -1,                                             # relative to month
+                   :year_delta => -2,                                              # relative to year
+                   :first_wday => 0,                                               # start of week 0 - Sunday, 1 - Monday
+                   :highlight_date => Date.new(2011,12,2),                         # highlight specific day
+                   :highlight_day => 2,                                            # highlight specific day
+                   :highlight_month => 12,                                         # highlight specific day
+                   :highlight_year => 2011,                                        # highlight specific day
+                   :highlight_between => Date.new(2011,12,1)..Date.new(2011,12,10) # highlight range of days
+                   ) %>
 ```
+
+Customize _public/stylesheets_ and copy to Your public directory.
+
 
--- app/helpers/calendar_view_helper.rb
+++ app/helpers/calendar_view_helper.rb
@@ -1,56 +1,102 @@
 module CalendarViewHelper
-    def calendar_cube(options={})
-        now = DateTime.now
-        first = Date.new(now.year,now.month,1)
-        last = Date.new(now.year,now.month,-1)
-        curr_week = first.cweek
-        html = "<h3>"
-        html << t(:month_names,:scope=>:date)[now.month]
-        html << "</h3><div class=\"content\">"
-        html << "<table class=\"side_calendar width100\">"
 
-        html << "<tr><td></td>"
-        1.upto(6) do |i|
-            html << "<td class=\"wday\">#{t(:abbr_day_names,:scope=>:date)[i]}</td>"
+    def color_follow_day(today,follow_day,highlight)
+        day = follow_day.day
+        c = ""
+        c << "this " if (highlight.class.eql?(Range) && highlight.include?(follow_day)) || follow_day == highlight
+        c << "today " if follow_day == today
+        c << "weekend " if [0,6].include?(follow_day.wday)
+        "<td class=\"#{c}\">#{day}</td>"
+    end
+
+    def wdays_names(first_wday)
+        html = ""
+        first_wday.upto(first_wday+6) do |i|
+            html << "<td class=\"wday\">#{t(:abbr_day_names,:scope=>:date)[i == 7 ? 0 : i]}</td>"
         end
-        html << "<td class=\"wday\">#{t(:abbr_day_names,:scope=>:date)[0]}</td>"
+        html
+    end
+
+    def calendar_square(options={})
+
+        now = DateTime.now
+        month = options[:month] ? options[:month] : now.month
+        year = options[:year] ? options[:year] : now.year
+
+        month_delta = options[:month_delta] ? options[:month_delta] : 0
+        year_delta = options[:year_delta] ? options[:year_delta] : 0
+
+        highlight = options[:highlight_date] ? options[:highlight_date] : nil
+        if options[:highlight_day] && options[:highlight_month] && options[:highlight_year]
+            highlight = Date.new(options[:highlight_year],options[:highlight_month],options[:highlight_day])
+        end
+        highlight = options[:highlight_between] if options[:highlight_between]
+
+        month = month + month_delta
+        year = year + year_delta
+        delta = month.divmod(12)
+
+        if delta[1].zero?
+            year = year + delta[0] -1
+            month = 12
+        else
+            year = year + delta[0]
+            month = delta[1]
+        end
+
+        first_wday = options[:first_wday] == 0 ? 0 : 1
+        last_wday = first_wday == 0 ? 6 : 0
+        first = Date.new(year,month,1)
+        last = Date.new(year,month,-1)
+        today = Date.new(now.year,now.month,now.day)
+
+        html = ""
+        html << "<div class=\"calendar square\">"
+        html << "<h3>"
+        html << t(:month_names,:scope=>:date)[month]
+        html << " #{year}" if year != now.year
+        html << "</h3>"
+        html << "<div class=\"content\">"
+        html << "<table>"
+
+        html << "<tr><td class=\"outside\">&nbsp;</td>"
+        html << wdays_names(first_wday)
         html << "</tr>"
 
-
-        html << "<tr>"
-        html << "<td class=\"week\">#{first.cweek}</td>"
-
-        (first.wday-1).downto(1) do |i|
-            prev = first - i
-            html << "<td class=\"off\">#{prev.day}</td>"
+        if first.wday != first_wday
+            (first_wday == 0 ? (first.wday == 0 ? 7 : first.wday) : (first.wday == 0 ? 7 : first.wday) - 1).downto(1) do |i|
+                prev = first - i
+                logger.custom(i,prev.to_s)
+                if prev.wday == first_wday
+                    html << "<tr>"
+                    html << "<td class=\"weeknum\">#{prev.cweek}</td>"
+                end
+                html << "<td class=\"outside\">#{prev.day}</td>"
+            end
         end
 
         (first.day).upto(last.day) do |i|
-            curr = Date.new(now.year,now.month,i)
-            if curr.wday == 1
-                html << "</tr>"
+            follow_day = Date.new(year,month,i)
+            if follow_day.wday == first_wday
                 html << "<tr>"
-                curr_week += 1
-                html << "<td class=\"week\">#{curr_week}</td>"
+                html << "<td class=\"weeknum\">#{follow_day.cweek}</td>"
             end
-            if now.day == i
-                html << "<td class=\"today\">#{i}</td>"
-            else
-                if curr.wday == 0 || curr.wday == 6
-                    html << "<td class=\"weekend\">#{i}</td>"
-                else
-                    html << "<td>#{i}</td>"
-                end
+            html << color_follow_day(today,follow_day,highlight)
+            if follow_day.wday == last_wday
+                html << "</tr>"
             end
         end
 
-        1.upto(7-last.wday) do |i|
-            post = last + i
-            html << "<td class=\"off\">#{post.day}</td>"
+        if last_wday != last.wday
+            1.upto(7-(first_wday == 0 ? last.wday+1 : last.wday)) do |i|
+                post = last + i
+                html << "<td class=\"outside\">#{post.day}</td>"
+            end
         end
-
         html << "</tr>"
-        html << "</table></div>"
-        html
+
+        html << "</table></div></div>"
+        html.html_safe
     end
+
 end
